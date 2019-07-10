c59f6de3d6fd7925f066df9c3202c7e8b5209121
Пользователь <Sviatoslv>
2019-07-10 11:02:20 +0300

    Updates

---
+++ precache-manifest.ba12ae273027d631328b254f23280961.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf786ff17e8a50181effc9890e20154",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "ae909a1dac56a03210c5",
+    "url": "/react_list-of-goods/static/css/main.9f0dcb69.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_list-of-goods/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ae909a1dac56a03210c5",
+    "url": "/react_list-of-goods/static/js/main.75186fdd.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
